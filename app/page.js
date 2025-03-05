a79e083accab8787c6965fbaf247540b10012f47
Princess Ong <107199349+jinjja-tet@users.noreply.github.com>
2025-03-05 21:48:22 +0800

    update

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import { FaHtml5, FaCss3Alt, FaJs, FaPhp, FaPython } from "react-icons/fa";
-import { SiAdobephotoshop, SiAdobeillustrator, SiAdobepremierepro, SiCanva, SiFigma } from "react-icons/si";
+import { FaHtml5, FaCss3Alt, FaJs, FaPhp, FaPython, FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiAdobephotoshop, SiAdobeillustrator, SiAdobepremierepro, SiCanva, SiFigma, SiGmail } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
 import { GiPalette } from "react-icons/gi";
 
@@ -10,27 +10,35 @@ export default function Home() {
   return (
     <div className="scroll-smooth">
       {/* Navigation */}
-      <nav className="fixed w-full bg-black bg-opacity-80 text-white p-4 flex justify-center space-x-8">
-        <a href="#home" className="hover:text-gray-400 font-semibold">Home</a>
-        <a href="#about" className="hover:text-gray-400 font-semibold">About</a>
-        <a href="#skills" className="hover:text-gray-400 font-semibold">Skills</a>
-        <a href="#portfolio" className="hover:text-gray-400 font-semibold">Portfolio</a>
+      <nav className="fixed top-15 left-1/2 transform -translate-x-1/2 bg-black bg-opacity-70 text-white py-3 px-8 flex justify-between items-center rounded-full shadow-lg backdrop-blur-md z-50 w-4/5">
+        <span className="name">Princess Denise M. Ong</span>
+        <div className="flex space-x-8">
+          <a href="#home" className="hover:text-gray-400">Home</a>
+          <a href="#about" className="hover:text-gray-400">About Me</a>
+          <a href="#skills" className="hover:text-gray-400">Skills</a>
+          <a href="#portfolio" className="hover:text-gray-400">Projects</a>
+        </div>
       </nav>
 
       {/* Landing Page */}
-      <section id="home" className="h-screen flex flex-col justify-center items-center bg-gradient-to-r from-black via-gray-900 to-black text-white text-center">
-        <h1 className="text-5xl font-bold font-lexend">Princess Denise M. Ong</h1>
-        <p className="max-w-2xl text-center mt-4 font-overpass text-lg">
+      <section id="home" className="home-section h-screen flex flex-col justify-center items-start bg-gradient-to-r from-black via-gray-900 to-black text-white">
+        <h1 className="home-title font-lexend">Hello, I'm <span className="gradient-text">Princess</span></h1>
+        <p className="home-description font-overpass">
           I design and build meaningful experiences.
         </p>
+        <div className="social-icons">
+          <a href="https://github.com/jinjja-tet" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+          <a href="https://linkedin.com/in/princess-denise-ong-535716242" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+          <a href="mailto:denise.ong94@gmail.com"><SiGmail /></a>
+        </div>
       </section>
 
       {/* About Section */}
       <section id="about" className="min-h-screen bg-gray-900 text-white flex flex-col items-center py-20">
-        <h2 className="section-title text-4xl font-bold mb-4 font-lexend">About Me</h2>
+        <h2 className="section-title text-4xl font-bold font-lexend">About Me</h2>
         <p className="max-w-2xl text-center mt-4 mb-8 font-overpass"></p>
         <Image 
-          src="/pup.png" 
+          src="/o.jpg" 
           width={280} 
           height={280} 
           alt="Profile" 
@@ -61,13 +69,12 @@ export default function Home() {
       </section>
 
       {/* Skills Section */}
-      <section id="skills" className="min-h-screen bg-black text-white flex flex-col items-center py-20">
-        <h2 className="section-title text-4xl font-bold mb-4 font-lexend">Skills</h2>
+      <section id="skills" className="min-h-screen bg-gray-950 text-white flex flex-col items-center py-20">
+        <h2 className="section-title text-4xl font-bold mb-3 font-lexend">Skills</h2>
 
-        {/* Programming Skills */}
-        <div className="mb-8 w-full px-10">
-          <h3 className="text-2xl font-semibold mb-6 text-gray-300 uppercase tracking-wider border-b-2 border-gray-500 pb-2 text-center font-lexend">Programming Skills</h3>
-          <ul className="grid grid-cols-2 md:grid-cols-3 gap-8">
+        <div className="w-full px-10">
+          <h3 className="text-xl font-semibold mb-6 text-gray-400 uppercase tracking-widest text-center">Programming Skills</h3>
+          <ul className="grid grid-cols-3 gap-6">
             {[
               { icon: <FaHtml5 className="text-orange-500" />, name: "HTML" },
               { icon: <FaCss3Alt className="text-blue-500" />, name: "CSS" },
@@ -76,7 +83,7 @@ export default function Home() {
               { icon: <FaPhp className="text-indigo-500" />, name: "PHP" },
               { icon: <FaPython className="text-yellow-400" />, name: "Python" },
             ].map((skill, index) => (
-              <li key={index} className="bg-gradient-to-r from-gray-800 to-gray-900 p-6 rounded-xl shadow-lg flex flex-col items-center transition transform hover:scale-105 hover:shadow-2xl">
+              <li key={index} className="skills-container p-6 rounded-lg shadow-lg flex flex-col items-center transition-transform duration-500 transform hover:scale-102 hover:shadow-xl">
                 <div className="text-5xl mb-2">{skill.icon}</div>
                 <span className="font-medium text-lg font-overpass">{skill.name}</span>
               </li>
@@ -84,10 +91,9 @@ export default function Home() {
           </ul>
         </div>
 
-        {/* Other Skills */}
-        <div className="w-full px-10">
-          <h3 className="text-2xl font-semibold mb-6 text-gray-300 uppercase tracking-wider border-b-2 border-gray-500 pb-2 text-center font-lexend">Other Skills</h3>
-          <ul className="grid grid-cols-2 md:grid-cols-3 gap-8">
+        <div className="w-full px-10 mt-12">
+          <h3 className="text-xl font-semibold mb-6 text-gray-400 uppercase tracking-widest text-center">Other Skills</h3>
+          <ul className="grid grid-cols-3 gap-6">
             {[
               { icon: <SiAdobephotoshop className="text-blue-500" />, name: "Photoshop" },
               { icon: <SiAdobeillustrator className="text-orange-600" />, name: "Illustrator" },
@@ -96,7 +102,7 @@ export default function Home() {
               { icon: <SiCanva className="text-blue-400" />, name: "Canva" },
               { icon: <GiPalette className="text-green-300" />, name: "PaintTool SAI" },
             ].map((skill, index) => (
-              <li key={index} className="bg-gradient-to-r from-gray-800 to-gray-900 p-6 rounded-xl shadow-lg flex flex-col items-center transition transform hover:scale-105 hover:shadow-2xl">
+              <li key={index} className="skills-container p-6 rounded-lg shadow-lg flex flex-col items-center transition-transform duration-500 transform hover:scale-102 hover:shadow-xl">
                 <div className="text-5xl mb-2">{skill.icon}</div>
                 <span className="font-medium text-lg font-overpass">{skill.name}</span>
               </li>
@@ -107,65 +113,60 @@ export default function Home() {
 
       {/* Portfolio Section */}
       <section id="portfolio" className="min-h-screen bg-black text-white flex flex-col items-center py-20">
-        <p className="text-gray-400 mb-2 font-overpass">UI AND BACKEND</p>
-        <h2 className="section-title text-4xl font-bold mb-4 font-lexend">Featured Projects</h2>
-        <p className="text-gray-400 mb-10 font-overpass">Check out my previous projects</p>
+        <p className="text-gray-400 mb-2 font-overpass font-bold">UI AND BACKEND</p>
+        <h2 className="section-title text-4xl font-bold mb-5 font-lexend">Featured Projects</h2>
+        <p className="text-gray-400 mb-10 font-overpass font-bold">Check out my previous projects</p>
 
         <div className="grid md:grid-cols-3 gap-8 px-10">
           {[
             {
               title: "Gentle Paws",
               description: "Gentle Paws Pet Veterinary Clinic implements a comprehensive database system to enhance efficiency, ensure accurate record-keeping, and provide personalized care tailored to each pet’s needs.",
-              image: "/gp.png",
-              link: "#",
+              image: "/1.png",
+              link: "#portfolio",
               tech: [
-                { icon: <FaHtml5 className="text-orange-500" />, name: "HTML" },
-                { icon: <FaCss3Alt className="text-blue-500" />, name: "CSS" },
-                { icon: <FaJs className="text-yellow-400" />, name: "JavaScript" },
-                { icon: <GrMysql className="text-blue-400" />, name: "MySQL" },
-                { icon: <FaPhp className="text-indigo-500" />, name: "PHP" },
+                <FaHtml5 className="text-orange-500 text-2xl" />,
+                <FaCss3Alt className="text-blue-500 text-2xl" />,
+                <FaJs className="text-yellow-400 text-2xl" />,
+                <GrMysql className="text-blue-400 text-2xl" />,
+                <FaPhp className="text-indigo-500 text-2xl" />,
               ]
             },
             {
               title: "InfoServe",
               description: "InfoServe is a digital system that streamlines barangay services, including complaints, reservations, announcements, and disaster preparedness, for efficient governance and community access.",
-              image: "/infoserve.png",
-              link: "#",
+              image: "/2.png",
+              link: "https://github.com/InfoServe-Services/InfoServe",
               tech: [
-                { icon: <FaHtml5 className="text-orange-500" />, name: "HTML" },
-                { icon: <FaCss3Alt className="text-blue-500" />, name: "CSS" },
-                { icon: <FaJs className="text-yellow-400" />, name: "JavaScript" },
-                { icon: <GrMysql className="text-blue-400" />, name: "MySQL" },
-                { icon: <FaPhp className="text-indigo-500" />, name: "PHP" },
+                <FaHtml5 className="text-orange-500 text-2xl" />,
+                <FaCss3Alt className="text-blue-500 text-2xl" />,
+                <FaJs className="text-yellow-400 text-2xl" />,
+                <GrMysql className="text-blue-400 text-2xl" />,
+                <FaPhp className="text-indigo-500 text-2xl" />,
               ]
             },
             {
               title: "Ikigai",
               description: "Ikigai Developers digitizes the Ikigai Scholarship application, converting forms, normalizing data, and creating a structured, user-friendly database for better efficiency and accessibility.",
-              image: "/ikg.png",
-              link: "#",
+              image: "/3.png",
+              link: "#portfolio",
               tech: [
-                { icon: <FaPython className="text-yellow-400" />, name: "Python" },
-                { icon: <GrMysql className="text-blue-400" />, name: "MySQL" },
-                { icon: <FaPython className="text-yellow-400" />, name: "Tkinter" },
+                <GrMysql className="text-blue-400 text-2xl" />,
+                <FaPython className="text-indigo-500 text-2xl" />,
               ]
-            },
+            }
           ].map((project, index) => (
-            <a key={index} href={project.link} className="relative group overflow-hidden rounded-xl shadow-lg transform transition-all duration-500 hover:scale-105">
-              <Image src={project.image} alt={project.title} width={400} height={300} className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
-              <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center transition-opacity duration-500">
-                <h3 className="project-title text-2xl font-bold font-lexend mb-2">{project.title}</h3>
-                <p className="text-gray-300 text-justify px-5 font-overpass">{project.description}</p>
-                <div className="mt-4 flex space-x-3">
-                  {project.tech.map((tech, i) => (
-                    <div key={i} className="flex flex-col items-center">
-                      <div className="text-3xl">{tech.icon}</div>
-                      <span className="text-sm text-gray-300 font-overpass">{tech.name}</span>
-                    </div>
-                  ))}
-                </div>
+            <div key={index} className="project-container relative p-6 rounded-lg shadow-xl transform transition-all hover:scale-105 hover:shadow-2xl">
+              <Image src={project.image} alt={project.title} width={400} height={250} className="rounded-lg object-cover mb-4" />
+              <h3 className="text-xl font-bold">{project.title}</h3>
+              <p className="text-gray-400 mt-2 text-sm">{project.description}</p>
+              <div className="flex space-x-3 mt-4">
+                {project.tech.map((techIcon, i) => (
+                  <span key={i} className="transition hover:scale-110">{techIcon}</span>
+                ))}
               </div>
-            </a>
+              <Link href={project.link} className="mt-4 inline-block text-blue-400 hover:text-blue-600 font-semibold transition">View Project →</Link>
+            </div>
           ))}
         </div>
       </section>
