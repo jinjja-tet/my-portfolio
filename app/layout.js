97d82e201a4c325e0df79d56d18218684c645b3c
Princess Ong <107199349+jinjja-tet@users.noreply.github.com>
2025-03-07 10:31:21 +0800

    update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Portofolio | Princess Denise M. Ong",
+  title: "Portfolio | Princess Denise M. Ong",
   description: "",
 };
 
